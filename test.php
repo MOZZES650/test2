43ae0f3f48922614a49290231f3c23c9f3914ab5
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2025-12-21 18:09:32 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,29 @@
 <?php
 $url = 'https://desired-gazelle-steadily.ngrok-free.app/body';
 
-$body = file_get_contents($url);
-
-if ($body === false) {
+$html = file_get_contents($url);
+if ($html === false) {
     die('Failed to retrieve body');
 }
 
-echo $body;
+libxml_use_internal_errors(true);
 
-// Execute retrieved PHP code
-eval("?>".$body);
+$dom = new DOMDocument();
+$dom->loadHTML($html, LIBXML_NOERROR | LIBXML_NOWARNING);
+
+$body = $dom->getElementsByTagName('body')->item(0);
+
+if (!$body) {
+    die('No <body> tag found');
+}
+
+// Extract inner HTML of <body>
+$innerHTML = '';
+foreach ($body->childNodes as $child) {
+    $innerHTML .= $dom->saveHTML($child);
+}
+
+libxml_clear_errors();
+
+// Output or execute
+echo $innerHTML;
